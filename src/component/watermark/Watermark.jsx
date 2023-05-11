13592f8f7dcba3a1874dc4fd798e6d1de1695c34
Mercy Dare <daremercy1@gmail.com>
2023-05-11 15:31:00 +0100

    picture watermark added

--- src/component/watermark/Watermark.jsx
+++ src/component/watermark/Watermark.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import "./watermark.css";
-// import { saveAs } from "file-saver";
-// import { Canvas } from "canvas";
 
 function Watermark() {
   const [image, setImage] = useState(null);
   const [watermarkText, setWatermarkText] = useState("");
+  const [watermarkImage, setWatermarkImage] = useState(null);
+  const [watermarkedImage, setWatermarkedImage] = useState(null);
+  const [watermarkType, setWatermarkType] = useState("text");
 
   const handleImageUpload = event => {
     const file = event.target.files[0];
@@ -16,37 +17,83 @@ function Watermark() {
     reader.readAsDataURL(file);
   };
 
+  const handleWatermarkType = e => {
+    setWatermarkType(e.target.value);
+  };
+
+  const handleWatermarkText = e => {
+    setWatermarkText(e.target.value);
+  };
+
+  const handleWatermarkImage = e => {
+    const file = e.target.files[0];
+    const reader = new FileReader();
+    reader.onload = () => {
+      setWatermarkImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
+
+useEffect(() => {
+  if (watermarkType === "text") {
+    const canvas = document.createElement("canvas");
+    const img = new Image();
+    img.src = image;
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      ctx.font = "bold 4rem Arial";
+      ctx.fillStyle = "rgb(255, 255, 255)";
+      ctx.textAlign = "center";
+      const x = canvas.width / 2;
+      const y = canvas.height - 50;
+      ctx.fillText(watermarkText, x, y);
+      const watermarkedUrl = canvas.toDataURL();
+      setWatermarkedImage(watermarkedUrl);
+    };
+  } else {
+    const canvas = document.createElement("canvas");
+    const img = new Image();
+    const overlayImage = new Image();
+    img.src = image;
+    overlayImage.src = watermarkImage;
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(img, 0, 0);
+      const overlayWidth = 100;
+      const overlayHeight =
+        (overlayWidth / overlayImage.width) * overlayImage.height;
+      const overlayX = (canvas.width - overlayWidth) / 2;
+      const overlayY = canvas.height - overlayHeight - 50;
+      ctx.drawImage(
+        overlayImage,
+        overlayX,
+        overlayY,
+        overlayWidth,
+        overlayHeight
+      );
+      const watermarkedUrl = canvas.toDataURL();
+      setWatermarkedImage(watermarkedUrl);
+    };
+  }
+}, [image, watermarkText, watermarkImage, watermarkType]);
+
+
   const handleDownload = () => {
-    if (image) {
-      const canvas = document.createElement("canvas");
-      const img = document.createElement("img");
-      img.src = image;
-      img.onload = () => {
-        canvas.width = img.width;
-        canvas.height = img.height;
-        const ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-        ctx.font = "bold 4rem Arial";
-        ctx.fillStyle = "rgba(0, 0, 0, 0.678)";
-        ctx.textAlign = "center";
-        const x = canvas.width / 2;
-        const y = canvas.height - 50;
-        ctx.fillText(watermarkText, x, y);
-        canvas.toBlob(blob => {
-          const url = URL.createObjectURL(blob);
-          const link = document.createElement("a");
-          link.href = url;
-          link.download = "watermarked-image.jpg";
-          link.click();
-          URL.revokeObjectURL(url);
-        }, "image/jpeg");
-      };
-    }
+    const link = document.createElement("a");
+    link.href = watermarkedImage;
+    link.download = "watermarked-image.jpg";
+    link.click();
   };
 
   return (
-    <div className="watrermark-container">
-      <form className="form-control">
+    <div className="watermark-container">
+      <h1>Watermark your image</h1>
+      <form className="form-control" onSubmit={e => e.preventDefault()}>
         <div className="input-container">
           <label htmlFor="image">Upload Image:</label>
           <input
@@ -57,25 +104,65 @@ function Watermark() {
           />
         </div>
 
-        <div className="input-container">
-          <input
-            id="text"
-            type="text"
-            onChange={e => setWatermarkText(e.target.value)}
-            value={watermarkText}
-            placeholder="Watermark text"
-          />
+        <div className="radio-group">
+          <p>Select watermark type</p>
+          <label>
+            <input
+              type="radio"
+              name="text"
+              value="text"
+              checked={watermarkType === "text"}
+              onClick={handleWatermarkType}
+            />
+            Text
+          </label>
+
+          <label>
+            <input
+              type="radio"
+              name="image"
+              value="image"
+              checked={watermarkType === "image"}
+              onClick={handleWatermarkType}
+            />
+            Image
+          </label>
         </div>
+
+        {watermarkType === "text" ? (
+          <div className="input-container">
+            <label htmlFor="text">Watermark text:</label>
+            <input
+              id="text"
+              type="text"
+              onChange={handleWatermarkText}
+              value={watermarkText}
+              placeholder="Watermark text"
+            />
+          </div>
+        ) : (
+          <div className="input-container">
+            <label htmlFor="image">Watermark image:</label>
+            <input
+              id="watermark-image"
+              type="file"
+              accept="image/*"
+              onChange={handleWatermarkImage}
+            />
+          </div>
+        )}
       </form>
 
-      <div className="watermark__preview">
-        {image && <img src={image} alt="" />}
-        <p className="watermark-text">{watermarkText}</p>
-      </div>
-
-      <a className="download-btn" download="image" onClick={handleDownload}>
-        Download
-      </a>
+      {image && watermarkType === "text" && watermarkText && (
+        <button className="download-btn" onClick={handleDownload}>
+          Download
+        </button>
+      )}
+      {image && watermarkType === "image" && watermarkImage && (
+        <button className="download-btn" onClick={handleDownload}>
+          Download
+        </button>
+      )}
     </div>
   );
 }
